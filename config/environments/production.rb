0362ad076c705ab0144fd3544517bf8bb047090f
ondrejsmetana <op.smetana@gmail.com>
2016-08-27 20:27:25 +0200

    init

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://chatapppidk.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://chatapppidk.herokuapp.com/' ]
+  config.action_cable.allowed_request_origins = [ 'https://chatapppidk.herokuapp.com' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
